a3da627ef731eac81bb98e80fa5b91a9bd6eaec9
Denys <herasymuk@ucu.edu.ua>
2020-02-08 15:23:50 +0200

    add json dict

--- parser_work_ua.py
+++ parser_work_ua.py
@@ -1,6 +1,6 @@
 import requests
 import json
-import pprint
+from pprint import pprint
 from bs4 import BeautifulSoup
 
 html_page = requests.get('https://www.work.ua/jobs-it/')
@@ -8,12 +8,13 @@ html_page = requests.get('https://www.work.ua/jobs-it/')
 soup = BeautifulSoup(html_page.text, 'html.parser')
 
 all_jobs_description = soup.find_all("div", {"class": "card card-hover card-visited wordwrap job-link js-hot-block"})
+string = '{}'
+table = json.loads(string)
 for i, job_description in enumerate(all_jobs_description):
     title_date = job_description.find_all("a")[0]['title']
     title, date = '', ''
     try:
         title, date = title_date.split(",")
-
     except:
         print('no 1 parameter')
 
@@ -29,8 +30,12 @@ for i, job_description in enumerate(all_jobs_description):
     company = job_description.find_all("img")[0]["alt"]
     url = job_description.a['href']
     url = "https://www.work.ua" + url
+    table[i + 1] = dict()
+    table[i + 1]["title"] = title
+    table[i + 1]["company"] = company
+    table[i + 1]["date"] = date
+    table[i + 1]["url"] = url
+    # if i == 4:
+    #     #     break
 
-    string = '{"1":}'
-    table = json.loads(string)
-    if i == 4:
-        break
+pprint(table)
