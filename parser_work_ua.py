cb7eea1a3c2d59322dabce461eeb1b30ec32d430
Denys <herasymuk@ucu.edu.ua>
2020-02-08 16:17:23 +0200

    make function for main pages

--- parser_work_ua.py
+++ parser_work_ua.py
@@ -1,41 +1,78 @@
+import time
+import os
 import requests
 import json
 from pprint import pprint
 from bs4 import BeautifulSoup
+from slugify import slugify
 
-html_page = requests.get('https://www.work.ua/jobs-it/')
 
-soup = BeautifulSoup(html_page.text, 'html.parser')
+def cache_page(url):
+    path = slugify(url) + ".html"
+    html_pages_path = os.path.join(os.getcwd(), 'work_ua_html_pages')
+    if path not in os.listdir(html_pages_path):
+        while True:
+            try:
+                r = requests.get(url)
+                break
+            except Exception as e:
+                print(e.with_traceback())
+                time.sleep(1)
+        with open(os.path.join(html_pages_path, path), "w", encoding="utf-8") as f:
+            f.write(r.text)
+    with open(os.path.join(html_pages_path, path), encoding="utf-8") as f:
+        text = f.read()
+    return text
 
-all_jobs_description = soup.find_all("div", {"class": "card card-hover card-visited wordwrap job-link js-hot-block"})
-string = '{}'
-table = json.loads(string)
-for i, job_description in enumerate(all_jobs_description):
-    title_date = job_description.find_all("a")[0]['title']
-    title, date = '', ''
-    try:
-        title, date = title_date.split(",")
-    except:
-        print('no 1 parameter')
 
-    city_tag = job_description.find(class_="add-top-xs")
+def parse_main_pages(url_work):
+    html_page = cache_page(url_work)
 
-    cities = ["Львів", "Київ", "Дніпро", "Одеса", "Харків", "Львов", "Киев", "Днепр", "Одесса", "Харьков"]
-    city = ''
-    for city_on_site in cities:
-        if str(city_tag).find(city_on_site):
-            city = city_on_site
-            break
+    soup = BeautifulSoup(html_page, 'html.parser')
 
-    company = job_description.find_all("img")[0]["alt"]
-    url = job_description.a['href']
-    url = "https://www.work.ua" + url
-    table[i + 1] = dict()
-    table[i + 1]["title"] = title
-    table[i + 1]["company"] = company
-    table[i + 1]["date"] = date
-    table[i + 1]["url"] = url
-    # if i == 4:
-    #     #     break
+    all_jobs_description = soup.find_all("div", {"class": "card card-hover card-visited wordwrap job-link js-hot-block"})
+    string = '{}'
+    table = json.loads(string)
+    for i, job_description in enumerate(all_jobs_description):
+        title_date = job_description.find_all("a")[0]['title']
+        title, date = '', ''
+        # if i == 3:
+        #     pprint(job_description)
+        try:
+            start_date = title_date.rfind(",")
+            title, date = title_date[:start_date], title_date[start_date:]
+        except:
+            print('no 1 parameter')
 
-pprint(table)
+        city_tag = job_description.find(class_="add-top-xs")
+
+        cities = ["Львів", "Київ", "Дніпро", "Одеса", "Харків", "Львов", "Киев", "Днепр", "Одесса", "Харьков"]
+        city = ''
+        for city_on_site in cities:
+            if str(city_tag).find(city_on_site):
+                city = city_on_site
+                break
+        company = ''
+        try:
+            company = job_description.find_all("img")[0]["alt"]
+        except:
+            print("company is not written")
+        url = job_description.a['href']
+        url = "https://www.work.ua" + url
+        table[i + 1] = dict()
+        table[i + 1]["title"] = title
+        table[i + 1]["company"] = company
+        table[i + 1]["date"] = date
+        table[i + 1]["url"] = url
+        # if i == 4:
+        #     #     break
+
+    return table
+
+
+if __name__ == "__main__":
+    n_vacancy = 1
+    for i in range(423):
+        url_work = 'https://www.work.ua/jobs-it/?advs=1&page='
+        url_work += str(i + 1)
+        pprint(parse_main_pages(url_work))
