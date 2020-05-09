519e3c7672614fdece4db85f9135f7e7b334b2e7
Denys <herasymuk@ucu.edu.ua>
2020-05-09 12:38:04 +0300

    check courses for skill

--- flack_app/application.py
+++ flack_app/application.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 
 # from routes import app
-from my_config import Config
+from flack_app.my_config import Config
 
 
 app = Flask(__name__)
@@ -29,7 +29,7 @@ skill_to_courses = db.Table('skill_to_courses', db.Model.metadata,
 
 class Profession(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
     skills = db.relationship("Skill",
                              secondary=profession_to_skill)
 
@@ -39,7 +39,7 @@ class Profession(db.Model):
 
 class Skill(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
     courses = db.relationship("Course",
                               secondary=skill_to_courses)
 
@@ -49,14 +49,14 @@ class Skill(db.Model):
 
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    course_title = db.Column(db.String(150), unique=True, nullable=False)
+    course_title = db.Column(db.String(300), unique=True, nullable=False)
     price = db.Column(db.String(80), unique=False, nullable=True)
-    image = db.Column(db.String(100), unique=False, nullable=True)
+    image = db.Column(db.String(500), unique=False, nullable=True)
     course_duration = db.Column(db.String(80), unique=False, nullable=True)
     number_of_students = db.Column(db.String(80), unique=False, nullable=True)
-    short_description = db.Column(db.String(2000), unique=False, nullable=True)
+    short_description = db.Column(db.String(5000), unique=False, nullable=True)
     long_description = db.Column(db.String(10000), unique=False, nullable=True)
-    url = db.Column(db.String(200), unique=True, nullable=True)
+    url = db.Column(db.String(300), unique=True, nullable=True)
 
     def __repr__(self):
         return '<Course - {};; {};; {};; {};; {};; {};; {};; {}>'.format(self.course_title, self.price, self.image,
@@ -229,10 +229,10 @@ def index():
 @app.route('/selected', methods=['POST', 'GET'])
 def selected():
     my_courses = []
-    data = request.form.getlist('checkbox')
+    # data = request.form.getlist('checkbox')
+    data = request.form
     print(data)
-    # data = request.form
-    # session['my_courses'] = list(data.keys())
+    session['my_courses'] = list(data.keys())
     for skill in courses.keys():
         for course in courses[skill]:
             if course['id'] in data:
--- flack_app/create_tables.py
+++ flack_app/create_tables.py
@@ -21,9 +21,9 @@ def create_db():
             else:
                 profession_name2 = ' '.join(profession_name.split("+"))
 
-            if db.session.query(Profession.id).filter_by(name=profession_name2).scalar() is not None:
-                print("Found in db")
-                continue
+            # if db.session.query(Profession.id).filter_by(name=profession_name2).scalar() is not None:
+            #     print("Found in db")
+            #     continue
 
             try:
                 p = Profession(name=profession_name2)
@@ -32,26 +32,35 @@ def create_db():
 
             except sqlite3.IntegrityError:
                 pass
+            except sqlalchemy.exc.IntegrityError:
+                pass
 
             print('profession_name', profession_name2)
             # write skills for the special profession in db
             for skill_name in json_skills_for_profession[profession_name]:
                 s = Skill(name=skill_name)
-                if db.session.query(Skill.id).filter_by(name=skill_name).scalar() is not None:
-                    print("Found in db")
+                try:
+                    if db.session.query(Skill.id).filter_by(name=skill_name).scalar() is not None:
+                        print("Found in db")
+                        continue
+                    db.session.add(s)
+                    db.session.commit()
+                except sqlite3.IntegrityError:
                     continue
-                db.session.add(s)
+                except sqlalchemy.exc.IntegrityError:
+                    continue
+                except sqlalchemy.exc.InvalidRequestError:
+                    continue
+
+            try:
+                db.session.rollback()
                 db.session.commit()
-
-            # try:
-            db.session.rollback()
-            db.session.commit()
-            db.create_all()
-            # except sqlite3.IntegrityError:
-            #     continue
-            # except sqlalchemy.exc.IntegrityError:
-            #     continue
-
+                db.create_all()
+            except sqlite3.IntegrityError:
+                continue
+            except sqlalchemy.exc.IntegrityError:
+                continue
+    num_course = 0
     for num_file, filename in enumerate(os.listdir(os.path.join('user_data', "courses_for_all_professions",
                                                                 "IT_courses"))):
         print(filename, "-" * 20)
@@ -62,8 +71,15 @@ def create_db():
             for course_title in json_courses_for_profession.keys():
                 # write courses for the special profession in db
                 course_data = json_courses_for_profession[course_title]
-                if db.session.query(Course.id).filter_by(course_title=course_title).scalar() is not None:
-                    print("Found in db")
+                try:
+                    if db.session.query(Course.id).filter_by(course_title=course_title).scalar() is not None:
+                        print("Found in db")
+                        continue
+                except sqlite3.IntegrityError:
+                    continue
+                except sqlalchemy.exc.IntegrityError:
+                    continue
+                except sqlalchemy.exc.InvalidRequestError:
                     continue
 
                 if isinstance(course_data["image"], dict):
@@ -98,18 +114,26 @@ def create_db():
 
                 except sqlite3.IntegrityError:
                     continue
+                except sqlalchemy.exc.IntegrityError:
+                    pass
+                except sqlalchemy.exc.InvalidRequestError:
+                    continue
 
                 # db.create_all()
+                num_course += 1
+                print("num_course", num_course)
                 print(course_title)
-                # try:
-                db.session.rollback()
-                db.session.commit()
-                db.create_all()
+                try:
+                    db.session.rollback()
+                    db.session.commit()
+                    db.create_all()
 
-                # except sqlite3.IntegrityError:
-                #     continue
-                # except sqlalchemy.exc.IntegrityError:
-                #     continue
+                except sqlite3.IntegrityError:
+                    continue
+                except sqlalchemy.exc.IntegrityError:
+                    continue
+                except sqlalchemy.exc.InvalidRequestError:
+                    continue
 
     job1 = Profession.query.filter_by(name='analyst').first()
     print(job1.skills)
